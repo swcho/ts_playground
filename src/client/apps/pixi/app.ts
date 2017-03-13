863ed10c911980e00ec03e33c6f6dd4c1d0f74ad
swcho <sungwoo.cho.dev@gmail.com>
2017-03-13 21:53:00 +0900

    Pixi.js Simple Example

--- src/client/apps/pixi/app.ts
+++ src/client/apps/pixi/app.ts
@@ -5,11 +5,18 @@ require('config-loader!./.config.ts');
 require('htmlout-loader!./en.html');
 const imgBunny = require('./bunny.png');
 
-var app = new PIXI.Application(800, 600, {backgroundColor : 0x1099bb});
+const width = 400;
+const height = 200;
+var app = new PIXI.Application(width, height, {backgroundColor : 0x1099bb}, true);
 document.body.appendChild(app.view);
 
 // create a new Sprite from an image path
 var bunny = PIXI.Sprite.fromImage('/' + imgBunny)
+
+var basicText = new PIXI.Text('하정아 어지러워 ~~');
+basicText.x = app.renderer.width / 2;
+basicText.y = app.renderer.height / 2;
+basicText.anchor.set(0.5);
 
 // center the sprite's anchor point
 bunny.anchor.set(0.5);
@@ -19,11 +26,38 @@ bunny.x = app.renderer.width / 2;
 bunny.y = app.renderer.height / 2;
 
 app.stage.addChild(bunny);
+app.stage.addChild(basicText);
 
+let diffX = 1;
+let diffY = 1;
+let rotationDir = 1;
+const table = ['1_1', '1_-1', '-1_-1', '-1_1'];
 // Listen for animate update
 app.ticker.add(function(delta) {
     // just for fun, let's rotate mr rabbit a little
     // delta is 1 if running at 100% performance
     // creates frame-independent tranformation
-    bunny.rotation += 0.1 * delta;
+    let x = bunny.position.x;
+    let y = bunny.position.y;
+    const prevUnitX = diffX;
+    const prevUnitY = diffY;
+    const prevIndexOf = table.indexOf(prevUnitX + '_' + prevUnitY);
+    if (x < 0 || width < x) {
+        diffX = -diffX;
+    }
+    if (y < 0 || height < y) {
+        diffY = -diffY;
+    }
+    const nextUnitX = diffX;
+    const nextUnitY = diffY;
+    const nextIndexOf = table.indexOf(nextUnitX + '_' + nextUnitY);
+    if (prevIndexOf !== nextIndexOf) {
+        console.log('index', prevIndexOf, nextIndexOf);
+        rotationDir = nextIndexOf - prevIndexOf;
+    }
+    console.log('dir', rotationDir)
+    bunny.rotation += rotationDir * 0.1 * delta;
+    basicText.rotation += rotationDir * 0.1 * delta;
+    bunny.position.x = x + diffX;
+    bunny.position.y = y + diffY;
 });
