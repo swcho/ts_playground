a48fca945166394b4b6b3c7dc4d4bd41bb12b0b0
swcho <sungwoo.cho.dev@gmail.com>
2017-07-09 07:22:58 +0900

    Try Key Movement

--- src/client/apps/raycaster/app.ts
+++ src/client/apps/raycaster/app.ts
@@ -297,19 +297,19 @@ class RayTracer {
 
 }
 
-let x = -1.0
+let x = -1.0;
+let y = 0.5;
 
 function defaultScene(): Scene {
-    // x += 0.01;
     return {
         things: [new Plane(new Vector(0.0, 1.0, 0.0), 0.0, Surfaces.checkerboard),
                  new Sphere(new Vector(0.0, 1.0, -0.25), 1.0, Surfaces.shiny),
-                 new Sphere(new Vector(x, 0.5, 1.5), 0.5, Surfaces.shiny)],
+                 new Sphere(new Vector(x, y, 1.5), 0.5, Surfaces.shiny)],
         lights: [{ pos: new Vector(-2.0, 2.5, 0.0), color: new Color(0.49, 0.07, 0.07) },
                  { pos: new Vector(1.5, 2.5, 1.5), color: new Color(0.07, 0.07, 0.49) },
                  { pos: new Vector(1.5, 2.5, -1.5), color: new Color(0.07, 0.49, 0.071) },
                  { pos: new Vector(0.0, 3.5, 0.0), color: new Color(0.21, 0.21, 0.35) }],
-        camera: new Camera(new Vector(3.0, 2.0, 4.0), new Vector(-1.0, 0.5, 0.0))
+        camera: new Camera(new Vector(5.0, 5.0, 5.0), new Vector(-1.0, 0.5, 0.0))
     };
 }
 
@@ -322,7 +322,6 @@ function exec() {
     return rayTracer.render(defaultScene(), ctx, 256, 256);
 }
 
-
 function draw() {
     requestAnimationFrame(() => {
         console.log('requestAnimationFrame')
@@ -331,4 +330,24 @@ function draw() {
     });
 }
 
-draw();
+// draw();
+
+document.addEventListener('keydown', (e) => {
+    console.log(e.code);
+    const handler = {
+        'ArrowRight': () => {
+            x += 0.05;
+        },
+        'ArrowLeft': () => {
+            x -= 0.05;
+        },
+        'ArrowUp': () => {
+            y += 0.05;
+        },
+        'ArrowDown': () => {
+            y -= 0.05;
+        },
+    };
+    handler[e.code]();
+    exec();
+});
