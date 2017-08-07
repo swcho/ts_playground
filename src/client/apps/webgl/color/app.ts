a73c10180bc327af2822be6683d654604abbd36a
swcho <sungwoo.cho.dev@gmail.com>
2017-08-07 09:20:51 +0900

    Add Ball

--- src/client/apps/webgl/color/app.ts
+++ src/client/apps/webgl/color/app.ts
@@ -15,40 +15,48 @@ console.log('WebGL Trials')
 // ref: https://codepen.io/cantelope/pen/zzmORP
 
 const elCanvas = document.getElementById('canvas-element-id') as HTMLCanvasElement;
-const webgl = new WebGL(elCanvas, CameraType.ORBIT);
+const webgl = new WebGL(elCanvas, CameraType.TRACKING);
 
 // webgl.setObject('pos', objects.HALF_SQUARE);
 webgl.addObject(objects.createFloor(60, 1));
 webgl.addObject(objects.createAxis(20));
 
-const objCone = objects.CONE6;
-objCone.position = {
-    x: 0,
-    y: 0,
-    z: 0,
-};
-TweenMax.to(objCone.position, 1, {
-    ...{x: 20} as any,
-    ease: Bounce,
-    yoyo: true,
-    repeat: -1,
-});
-webgl.addObject(objCone);
+// const objCone = objects.CONE6;
+// objCone.position = {
+//     x: 0,
+//     y: 0,
+//     z: 0,
+// };
+// TweenMax.to(objCone.position, 1, {
+//     ...{x: 20} as any,
+//     ease: Bounce,
+//     yoyo: true,
+//     repeat: -1,
+// });
+// webgl.addObject(objCone);
 
-const sphere: Object = require('../sphere2.json');
-sphere.position = {
-    x: 0,
-    y: 0,
-    z: 0,
-};
-TweenMax.to(sphere.position, 1, {
-    ...{z: 20} as any,
-    ease: Bounce,
-    yoyo: true,
-    repeat: -1,
-});
-webgl.addObject(sphere as Object);
-webgl.addObject(require('../complexcube.json'));
+// const sphere: Object = require('../sphere.json');
+// sphere.position = {
+//     x: 0,
+//     y: 0,
+//     z: 0,
+// };
+// TweenMax.to(sphere.position, 1, {
+//     ...{z: 20} as any,
+//     ease: Bounce,
+//     yoyo: true,
+//     repeat: -1,
+// });
+// webgl.addObject(sphere as Object);
+
+// Cube
+// webgl.addObject(require('../complexcube.json'));
+
+// Balls
+const BALL = require('../ball.json');
+for (let i = 0; i<10; i += 1) {
+
+}
 
 const home = vec3.create();
 home.set([0.0, 0.0, 5.0])
