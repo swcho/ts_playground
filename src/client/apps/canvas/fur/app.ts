5c6f02b7629f88e2a7b39dea9ca77da08a4643b5
swcho <sungwoo.cho.dev@gmail.com>
2017-08-21 15:44:29 +0900

    Fur Refactoring

--- src/client/apps/canvas/fur/app.ts
+++ src/client/apps/canvas/fur/app.ts
@@ -4,77 +4,6 @@ import 'htmlout-loader!./en.html';
 console.log(__filename);
 console.log('hi');
 
-const container = 'body';
-const size = {
-    x: $(container).width(),
-    y: $(container).height()
-};
-
-// const color = [157, 208, 136];
-const color = [99, 99, 99];
-
-const resolution_scale = 1;
-
-const bunchSize = 20;
-const perlinSpread = 50;
-
-const hairdist = 20;
-const exS = 4;
-
-const canvas = $('<canvas/>').attr({ width: size.x * resolution_scale, height: size.y * resolution_scale }).appendTo(container);
-const context = (canvas.get(0) as HTMLCanvasElement).getContext('2d');
-
-const startTime = new Date().getTime();
-let currentTime = 0;
-
-const wind = [];
-function initWind() {
-    for (let x = 0; x <= size.x / bunchSize + exS; x++) {
-        wind.push([]);
-        for (let y = 0; y <= size.y / bunchSize + exS; y++) {
-            wind[x].push([]);
-        }
-    }
-}
-initWind();
-
-declare const noise;
-
-function updateWind() {
-    for (let x = 0; x <= size.x / bunchSize + exS; x++) {
-        for (let y = 0; y <= size.y / bunchSize + exS; y++) {
-            // wind[x][y] = noise.perlin3((currentTime + x) / perlinSpread, (currentTime + y) / perlinSpread, currentTime * 1.25);
-            // wind[x][y] = noise.perlin2((currentTime + x) / perlinSpread, currentTime * 1.25);
-            wind[x][y] = noise.simplex2((currentTime + x) / perlinSpread, currentTime * 1.25);
-        }
-    }
-}
-console.log(wind);
-
-let hairs = [];
-let hair = function (position, shape, shade) {
-    this.position = position;
-    this.drawShape = shape;
-    this.shade = shade;
-
-    this.draw = function () {
-        let windr = wind[Math.floor(this.position.x / bunchSize)][Math.floor(this.position.y / bunchSize)];
-        if (!windr) {
-            windr = 0;
-        }
-        context.save();
-        context.translate((this.position.x - hairdist * 2) * resolution_scale, (this.position.y - hairdist * 2) * resolution_scale);
-        context.rotate(windr * (Math.PI / 2));
-        this.light = Math.floor(40 * (1 + windr)) + this.shade;
-        this.drawShape(this.light);
-        context.restore();
-
-    };
-
-    this.id = hairs.length;
-    hairs.push(this);
-};
-
 const requestFrame = (function () {
     return window.requestAnimationFrame ||
         window.webkitRequestAnimationFrame ||
@@ -84,29 +13,113 @@ const requestFrame = (function () {
         };
 })();
 
-function hairShape(light) {
-    context.fillStyle = 'rgb(' + (color[0] + light) + ',' + (color[1] + light) + ',' + (color[2] + light) + ')';
-    context.fillRect(0, 80 * resolution_scale, 2 * resolution_scale, -80 * resolution_scale);
+const container = 'body';
+const size = {
+    x: $(container).width(),
+    y: $(container).height()
+};
+
+const color = [157, 208, 136];
+// const color = [99, 99, 99];
+
+const RESOLUTION_SCALE = 1;
+
+const BUNCH_SIZE = 20;
+const PERLIN_SPEAD = 50;
+
+const HAIR_DISTANCE = 10;
+const exS = 4;
+
+const canvas = $('<canvas/>').attr({ width: size.x * RESOLUTION_SCALE, height: size.y * RESOLUTION_SCALE }).appendTo(container);
+const context = (canvas.get(0) as HTMLCanvasElement).getContext('2d');
+
+const startTime = new Date().getTime();
+let currentTime = 0;
+
+const wind = [];
+function initWind() {
+    for (let x = 0; x <= size.x / BUNCH_SIZE + exS; x++) {
+        wind.push([]);
+        for (let y = 0; y <= size.y / BUNCH_SIZE + exS; y++) {
+            wind[x].push([]);
+        }
+    }
+}
+initWind();
+
+declare const noise;
+
+function updateWind() {
+    for (let x = 0; x <= size.x / BUNCH_SIZE + exS; x++) {
+        for (let y = 0; y <= size.y / BUNCH_SIZE + exS; y++) {
+            // wind[x][y] = noise.perlin3((currentTime + x) / PERLIN_SPEAD, (currentTime + y) / PERLIN_SPEAD, currentTime * 1.25);
+            // wind[x][y] = noise.perlin2((currentTime + x) / PERLIN_SPEAD, currentTime * 1.25);
+            wind[x][y] = noise.simplex2((currentTime + x) / PERLIN_SPEAD, currentTime * 1.25);
+        }
+    }
 }
 
+const hairs: Hair[] = [];
 
-for (let x = 0; x < size.x / hairdist + exS; x++) {
-    for (let y = 0; y < size.y / hairdist + exS; y++) {
-        new hair({ x: x * hairdist + Math.floor(Math.random() * hairdist), y: y * hairdist + Math.floor(Math.random() * hairdist) }, hairShape, Math.floor(Math.random() * 30));
+interface Position {
+    x: number;
+    y: number;
+}
+
+class Hair {
+    private id;
+    constructor(private position: Position, private drawShape, private shade) {
+        this.id = hairs.length;
+        hairs.push(this);
+    }
+
+    draw() {
+        const {
+            x,
+            y,
+        } = this.position;
+        let windr = wind[Math.floor(x / BUNCH_SIZE)][Math.floor(y / BUNCH_SIZE)];
+        if (!windr) {
+            windr = 0;
+        }
+        context.save();
+        context.translate(
+            (this.position.x - HAIR_DISTANCE * 2) * RESOLUTION_SCALE,
+            (this.position.y - HAIR_DISTANCE * 2) * RESOLUTION_SCALE);
+        context.rotate(windr * (Math.PI / 2));
+        // context.rotate(windr * Math.PI * 2);
+        const light = Math.floor(40 * (1 + windr)) + this.shade;
+        this.drawShape(light);
+        context.restore();
+    }
+};
+
+function hairShape(light) {
+    context.fillStyle = 'rgb(' + (color[0] + light) + ',' + (color[1] + light) + ',' + (color[2] + light) + ')';
+    context.fillRect(0, 80 * RESOLUTION_SCALE, 1 * RESOLUTION_SCALE, -80 * RESOLUTION_SCALE);
+}
+
+for (let x = 0; x < size.x / HAIR_DISTANCE + exS; x++) {
+    for (let y = 0; y < size.y / HAIR_DISTANCE + exS; y++) {
+        new Hair({
+            x: x * HAIR_DISTANCE + Math.floor(Math.random() * HAIR_DISTANCE),
+            y: y * HAIR_DISTANCE + Math.floor(Math.random() * HAIR_DISTANCE),
+        },
+        hairShape,
+        Math.floor(Math.random() * 30));
     }
 }
 
 function render() {
     let now = new Date().getTime();
     currentTime = (now - startTime) / 10000;
-    context.clearRect(0, 0, size.x * resolution_scale, size.y * resolution_scale);
+    context.clearRect(0, 0, size.x * RESOLUTION_SCALE, size.y * RESOLUTION_SCALE);
 
     updateWind();
     for (let i = 0; i <= hairs.length; i++) {
         if (hairs[i]) {
             hairs[i].draw();
         }
-
     }
 
     requestFrame(render);
