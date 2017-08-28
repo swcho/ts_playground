cfd2a175ce76d9d077473977078af54b4fceefca
swcho <sungwoo.cho.dev@gmail.com>
2017-08-28 21:02:59 +0900

    react-motion: simple

--- src/client/apps/react/simple.tsx
+++ src/client/apps/react/simple.tsx
@@ -1,12 +1,57 @@
 
 import * as React from 'react';
+import styled from 'styled-components';
+import {Motion, spring} from 'react-motion';
 
-export class SimpleMotion extends React.Component<{}, {}> {
+const Bar = styled.div`
+    border-radius: 4px;
+    background-color: rgb(240, 240, 232);
+    position: relative;
+    margin: 5px 3px 10px;
+    width: 450px;
+    height: 50px;
+`;
 
-    render() {
-        return (
-            <div>Simple</div>
-        );
+const Block = styled.div`
+    position: absolute;
+    width: 50px;
+    height: 50px;
+    border-radius: 4px;
+    background-color: rgb(130, 181, 198);
+`;
+
+export class SimpleMotion extends React.Component<{}, {
+    on: boolean;
+}> {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            on: false,
+        };
     }
 
-}
+    render() {
+        const {
+            on
+        } = this.state;
+        return (
+            <div>
+                <h1>Simple Transition</h1>
+                <Bar
+                    onClick={
+                        () => this.setState({on: !on})
+                    }
+                >
+                    <Motion
+                        defaultStyle={{left: 0}}
+                        style={{left: spring(on ? 400 : 0)}}
+                        onRest={() => console.log('onRest')}
+                    >
+                        {newStyle => <Block style={newStyle}></Block>}
+                    </Motion>
+                </Bar>
+            </div>
+        );
+    }
+};
