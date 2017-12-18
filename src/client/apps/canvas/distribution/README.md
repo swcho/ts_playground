6ad763ef96a16138a1e88878d902a8de68e4b6fd
swcho <sungwoo.cho.dev@gmail.com>
2017-12-18 17:44:10 +0900

    CANVAS: Distribution Comment

--- src/client/apps/canvas/distribution/README.md
+++ src/client/apps/canvas/distribution/README.md
@@ -1,5 +1,10 @@
-title
+Distribution
 ===
 
-[Origin]()
+[Origin](https://codepen.io/mcdorli/pen/GvyYQV)
 
+Even distribution.
+
+<https://stackoverflow.com/questions/42603609/even-distribution-of-random-points-in-2d>
+
+force = |(i - j)| / 
--- src/client/apps/canvas/distribution/app.ts
+++ src/client/apps/canvas/distribution/app.ts
@@ -8,11 +8,13 @@ console.log(__filename);
 
 import dat = require('dat-gui');
 const CONFIG = {
-    division: 25,
+    division: 50000,
+    maxSize: 200,
 };
 
 let gui = new dat.GUI();
-gui.add(CONFIG, 'division', 0.01, 50).step(0.01);
+gui.add(CONFIG, 'division', 10000, 100000).step(100);
+gui.add(CONFIG, 'maxSize', 100, 300).step(1);
 
 let c = document.getElementById('canvas') as HTMLCanvasElement;
 let ctx = c.getContext('2d');
@@ -48,80 +50,89 @@ c.addEventListener('mouseup', (e) => {
     holding = -1;
 });
 
-let nodes = [];
+let nodes: Vec[] = [];
 for (let i = 0; i < 200; i++) {
     nodes.push(mul(normalize(vec(Math.random() - 0.5, Math.random() - 0.5)), Math.random() * 200));
 }
 
 function loop() {
-    let newNodes = [];
+    let newNodes: Vec[] = [];
     for (let i = 0; i < nodes.length; i++) {
         let p = nodes[i];
         let force = vec(0, 0);
         if (i !== holding) {
             for (let j = 0; j < nodes.length; j++) {
                 if (i !== j) {
-                    let direction = sub(nodes[j], p);
+                    // let direction = sub(nodes[j], p);
+                    let direction = sub(p, nodes[j]);
                     let dist = mag(direction);
-                    force = add(force, mul(normalize(mul(direction, -1)), 1 / (dist * dist * dist) * 50000));
+                    // force = add(force, mul(normalize(mul(direction, -1)), 1 / (dist * dist * dist) * CONFIG.division));
+                    force = add(force, mul(normalize(direction), 1 / (dist * dist * dist) * CONFIG.division));
+                    // force = add(force, mul(normalize(mul(direction, -1)), 1 / dist * CONFIG.division));
                 }
             }
         }
 
         if (mag(force) > 1)
             force = mul(normalize(force), 1);
-        let newPos = add(p, force);
-        if (mag(newPos) > 200)
-            newPos = mul(normalize(newPos), 200);
+        let newPos: Vec = add(p, force);
+        if (mag(newPos) > CONFIG.maxSize)
+            newPos = mul(normalize(newPos), CONFIG.maxSize);
         newNodes.push(newPos);
     }
     nodes = newNodes;
 
     ctx.clearRect(-250, -250, 500, 500);
     ctx.beginPath();
-    for (let n of nodes) {
+    nodes.forEach(function(n, i) {
         ctx.moveTo(n.x, n.y);
         ctx.arc(n.x, n.y, 7, 0, 2 * Math.PI);
-    }
+        ctx.fillText('' + i, n.x, n.y);
+    });
     ctx.fill();
     requestAnimationFrame(loop);
 }
 
 // Vector helper functions
 
-function vec(x, y) {
+interface Vec {
+    x: number;
+    y: number;
+}
+
+function vec(x, y): Vec {
     return {
         x: x,
         y: y
     };
 }
 
-function add(v1, v2) {
+function add(v1: Vec, v2: Vec): Vec {
     return {
         x: v1.x + v2.x,
         y: v1.y + v2.y
     };
 }
 
-function sub(v1, v2) {
+function sub(v1: Vec, v2: Vec): Vec {
     return {
         x: v1.x - v2.x,
         y: v1.y - v2.y
     };
 }
 
-function mul(v, n) {
+function mul(v: Vec, n: number): Vec {
     return {
         x: v.x * n,
         y: v.y * n
     };
 }
 
-function mag(v) {
+function mag(v: Vec): number {
     return Math.hypot(v.x, v.y);
 }
 
-function normalize(v) {
+function normalize(v: Vec): Vec {
     let m = mag(v);
     return mul(v, 1 / m);
 }
